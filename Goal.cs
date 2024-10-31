01bf6d694b9c5beaebd454a779e135bb9b508906
mikeyamashita <48137635+mikeyamashita@users.noreply.github.com>
2024-10-31 13:43:12 -0600

    habit times

--- Goal.cs
+++ Goal.cs
@@ -2,8 +2,8 @@ public class Goal
 {
     public string? Name { get; set; }
     public string? Description { get; set; }
-    public DateTime? StartDate { get;}
-    public DateTime? EndDate { get;}
+    public DateTime? StartDate { get; set;}
+    public DateTime? EndDate { get; set;}
     public List<Habit>? Habits { get; set; }
     public List<Milestone>? Milestones { get; set; }
     public bool IsComplete { get; set; }
--- Habit.cs
+++ Habit.cs
@@ -2,6 +2,7 @@ public class Habit
 {
     public string? Name { get; set; }
     public List<string>? DatesCompleted { get; set; }
+    public List<string>? Times { get; set; }
     public bool IsComplete { get; set; }
 
 }
